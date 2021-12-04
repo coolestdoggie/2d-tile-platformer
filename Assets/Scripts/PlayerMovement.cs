767ace46ab01af6dbc1fc0073b8492a060da820c
coolestdoggie <coolestdoggie1@gmail.com>
2021-12-04 13:17:51 +0100

    [FIX] PlayerMovement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,15 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float runSpeed = 10f;
     private Vector2 moveInput;
+    private Rigidbody2D rdbd2d;
+
+    private void Awake()
+    {
+        rdbd2d = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
         
@@ -13,10 +22,16 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+        Move();
     }
 
-   void OnMove(InputValue value)
+    private void Move()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed * Time.deltaTime, rdbd2d.velocity.y);
+        rdbd2d.velocity = playerVelocity;
+    }
+
+    void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
