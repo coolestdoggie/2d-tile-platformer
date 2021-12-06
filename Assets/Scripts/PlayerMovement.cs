c3000beba2c583534c9fa2bb58f1c988175fa842
coolestdoggie <coolestdoggie1@gmail.com>
2021-12-06 15:23:08 +0100

    [ADD][FIX] FlipSprite() in PlayerMovement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -19,17 +20,26 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Run();
+        FlipSprite();
     }
     
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
     }
     
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rdbd2d.velocity.y);
         rdbd2d.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool isPlayerHasHorSpeed = Mathf.Abs(rdbd2d.velocity.x) > Mathf.Epsilon;
+        if (isPlayerHasHorSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rdbd2d.velocity.x), 1f);
+        }
     }
 }
